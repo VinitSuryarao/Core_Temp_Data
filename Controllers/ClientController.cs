a1a02282116a4fd6f5d39c83a07d5b4ea80505d4
Vinit Suryarao <60574327+VinitSuryarao@users.noreply.github.com>
2020-03-15 19:50:19 +0530

    Add files via upload

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Temp_Data.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            base.TempData["Name"] = "Vinit Suryarao";
+            base.TempData["Age"] = 25;
+
+            return View();
+        }
+
+        public IActionResult FirstRequest()
+        {
+            // Only Save Data
+            TempData.Keep();
+
+            // Save + Carry Data
+            var Name1 = TempData.Peek("Name");
+
+            return View();
+        }
+
+
+        public IActionResult SecondRequest()
+        {
+            // Only Save Data
+            TempData.Keep();
+
+            // Save + Carry Data
+            var Name2 = TempData.Peek("Name");
+
+            return View();
+        }
+    }
+}
